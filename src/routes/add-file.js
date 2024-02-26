bcdff325401279e5808d19825206c2797391f188
Maxime <maxime.barachet@gmail.com>
2024-02-26 23:09:50 +0100

    image upload with token

--- src/routes/add-file.js
+++ src/routes/add-file.js
@@ -1,9 +1,10 @@
 // routes/add-file.js
 const express = require("express");
 const router = express.Router();
+const passport = require('passport'); // Import Passport
 const fileController = require("../controllers/fileController");
 
-// Route POST pour télécharger un fichier
-router.post("/add-file", fileController.uploadFile, fileController.handleFileUpload);
+// Secure the route using Passport authentication middleware
+router.post("/add-file", passport.authenticate('jwt-verify', { session: false }), fileController.uploadFile, fileController.handleFileUpload);
 
 module.exports = router;
